4514687a5b57acaa1b7e25458671e3b3a8d1885c
Alex <alex.kastsiukovich@gmail.com>
2022-02-07 22:41:01 +0300

    task5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function clearPreloader(){
+function clearPreloader() {
     var preloader = document.querySelector('#preloader');
     var content = document.querySelector('#site');
     preloader.style.display = "none";
@@ -10,24 +10,24 @@ setTimeout(clearPreloader, 1000);
 // task1
 var quad = document.querySelector('#quad');
 
-quad.addEventListener('dblclick', function(e){
-   console.log(e.target)
-   quad.style.backgroundColor = 'blue';
+quad.addEventListener('dblclick', function (e) {
+    console.log(e.target)
+    quad.style.backgroundColor = 'blue';
 })
 
 // task2
 var images = document.querySelector('.club-image');
 
-images.addEventListener('click', function(e){
+images.addEventListener('click', function (e) {
     alert(e.target.src)
 })
 
 // task3
 var linkV = document.querySelector('.vrapper1');
 var span = document.getElementsByClassName('.one')
-linkV.addEventListener('mouseover', function(e){
+linkV.addEventListener('mouseover', function (e) {
     // console.log(e.target.tagName === "A")
-    if(e.target.tagName === "A"){
+    if (e.target.tagName === "A") {
         var val = e.target.href;
         console.log(val)
         e.target.nextElementSibling.innerText = ` ${val}`;
@@ -37,24 +37,24 @@ linkV.addEventListener('mouseover', function(e){
 var card = document.querySelector('.card');
 var body1 = window;
 
-body1.addEventListener('keydown', function(event){
+body1.addEventListener('keydown', function (event) {
     console.log(event.key);
-    if(event.key === "Escape"){
+    if (event.key === "Escape") {
         card.style.display = 'none';
     };
 })
 // task5
-var selec = document.getElementsByClassName('browser-default');
 
-selec.addEventListener('select', function(e){
-    var tex = document.getElementsByClassName('club-image')[1];
-
-    
-    console.log(e.target)
+var selec1 = document.getElementsByClassName('browser-default')[0];
+var text = document.querySelector('#current-result');
+selec1.addEventListener('change', function (e) {
+    text.innerHTML = e.target.value;
 })
+// task6
 
 
-    
+
+
 
 
 
@@ -72,3 +72,16 @@ var searchList = [
     'Пачакун',
     'Ленивец'
 ]
+
+
+
+// Функция рандома
+function randomMy(a, b) {
+    console.log(Math.floor(Math.random() * (b - a + 1)) + a)
+}
+randomMy(1, 50)
+
+
+
+
+
